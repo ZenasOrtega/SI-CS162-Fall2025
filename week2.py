8368986a12775cfe3ce7f7a7518b75d36e8d8080
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2025-10-05 16:06:29 -0700

    Update week2.py

--- week2.py
+++ week2.py
@@ -3,19 +3,24 @@
 def main() -> None:
     #make a function that is able to take two user intiger inputs, and add them together, print the results
     #in the given comment lines answer the small prompts 
-    X = int(input('what is the first number you want to add?' ))
-    Y = int(input('what is the second number you want to add?' ))
+    X = int(input('what is the first number you want to add?'))
+    Y = int(input('what is the second number you want to add?'))
 
     #start here
 
-    #are X and Y in main the same variables as X and Y in your function?: (your answer)
-    #Why is the return type int?: 
-
+    #What does each part of the function defenition tell us? 
+    #are X and Y in main the same variables as X and Y in your function?: 
+    
     #make a loop that adds A by X, Y times
-    #A = int(input('What number should we add to? '))
+    #A = int(input('What number should we add to? ')
+
+    #same concept but with the other type of loop
+
+    #When do we use either loop?
 
 if __name__ == '__main__':
     main()
+
 
 
 
