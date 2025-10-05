b25d51c83209f99eb12770d8166e5f8d59a5b76e
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2025-10-05 15:34:50 -0700

    Update week2.py

--- week2.py
+++ week2.py
@@ -1,7 +1,16 @@
-#hello test
+#perameter names should be called X and Y
 
 def main() -> None:
-    print('Hello World!')
+    #make a function that is able to take two user intiger inputs, and add them together, print the results
+    #in the given comment lines answer the small prompts 
+    X = input('what is the first number you want to add?')
+    Y = input('what is the second number you want to add?')
+
+    #are X and Y in main the same variables as X and Y in your function?: (your answer)
+    #what happens if you use None as the return type?:
+
+    #make a loop that adds A by X, Y times
+    #A = input('What number should we add to?')
 
 if __name__ == '__main__':
     main()
