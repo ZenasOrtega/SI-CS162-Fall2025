de63e9d5ab181a4bb48c9cdbad149f49ad2b6f8d
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2025-11-21 14:39:43 -0800

    Create week8.py

---
+++ week8.py
@@ -0,0 +1,17 @@
+#make a recursive fxn that is able to add incrementing numbers X times, where X is the user input, retunr the sum
+  #ex say IP = 3, then it be 1+2+3 = 6, return 6
+
+#make a recursive fxn that can identify if a string is a palindrome (attempt first w/out notes :)
+  #ex "racecar" is backwards for "racecar" 
+
+#make a recursive fxn for the fibonacci sequence (same here, attempt first w/out notes
+  #ex 0+1+1+2+3+5+8 (the next number is the sum of the previous two)
+
+#make a recursive fxn that is able to print out the factors of a user inputted number
+  #ex IP = 20, then it prints: 1, 2 , 4 , 5, 10, 20
+
+
+def main():
+
+if __name__ == "__main__":
+    main()
