6aad240087e297fd1801c95026af013c2009f267
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-01-23 13:53:32 -0800

    Update week3.py

--- week3.py
+++ week3.py
@@ -1,4 +1,39 @@
 def main() -> None:
+  #match the following errors
+  #Occurs when attempting to access a file that does not exist.
+    #
+  #Occurs when attempting to cast an object to a type that it is not compatible with.
+    #
+  #Occurs when a function receives an argument of the correct type but an inappropriate value.
+    #
+  #Occurs during invalid arithmetic operations, such as division by zero.
+    #
+  #Occurs when attempting to access an array element using an index that is outside the valid range.
+    #
+  #Occurs when an operation is performed on a value of the wrong data type.
+    #
+  #Occurs when attempting to convert a string that does not represent a valid number into a numerical type.
 
-if __name__ = "__main__":
+  #word bank
+    #FileNotFoundException
+    #ArithmeticException
+    #NumberFormatException 
+    #ClassCastException
+    #TypeError (Python) 
+    #ValueError (Python)
+    #ArrayIndexOutOfBoundsException
+
+  #make a .txt file and copy and paste this into it (remove the #'s):
+    #Name, Media Type 
+    #Black clover, Show, 
+    #Pokemon, Game
+    #Happy Gilmore, Movie
+    #Starlight, Song
+
+  #TODO:
+    #read each line from the csv and print it out
+
+  #whats the difference between write and appending to a text file?
+
+if __name__ = '__main__':
   main()
