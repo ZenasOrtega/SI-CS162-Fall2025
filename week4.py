f1126437783b6d00df69c61538c3cfe5a771bc82
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-01-29 14:36:26 -0800

    Update week4.py

--- week4.py
+++ week4.py
@@ -1,2 +1,19 @@
+#in a seprate file make a car class, it should contatin the following:
+  #make as a string
+  #number of wheels as an integer 
+  #price as a float
+
+#fxn to calc to calculate monthly payment using the expression: price/(users_wage * 20)
+  #make sure to account for all possible inputs (-#s to +#s)
+
+#fxn to check the conditions of tires
+  #the user will inpuit each individual tires condition, if in 'bad' call some fxn
+
+
 def main() -> None:
-  
+  user_wage = flaot(input('whats your monthly payment'))
+  #start here
+
+
+if __name__ == '__main__':
+    main()
