c37feca19629a243c0086170e129be05e8286899
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-02-13 13:21:01 -0800

    Create week6.py

---
+++ week6.py
@@ -0,0 +1,34 @@
+class Food:
+  Meal_time: str
+  def __init__(self, x: str) - > None:
+    self.Meal = x
+
+def change_list_values(list_from_main: list[int]) -> none:
+  lsit_from_main[0] = 123456789
+
+def main() -> None:
+  #answer the following:
+  x = 5
+    #whats x to 5?:
+
+  y = x
+  print(id(x))
+  print(id(y))
+  print("\n")
+  x = 1
+  print(id(x))
+  print(id(y))
+    #what do you notice about the ID's? 
+
+  list[1, 2, 3, 4]
+  for i in list
+    print(i)
+
+  change_list_values(list)
+  for i in list
+    print(i)
+  
+
+  Pancakes = Food("Breakfast")
+  Steak = Food("Dinner)
+
