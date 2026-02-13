84d8939499de06d7c0d526b54309ca1ada343020
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-02-13 13:50:24 -0800

    Update week6.py

--- week6.py
+++ week6.py
@@ -1,34 +1,42 @@
-class Food:
-  Meal_time: str
-  def __init__(self, x: str) - > None:
-    self.Meal = x
-
-def change_list_values(list_from_main: list[int]) -> none:
-  lsit_from_main[0] = 123456789
+def change_list_values(list_from_main: list[int]) -> None:
+    list_from_main[0] = 12
 
 def main() -> None:
-  #answer the following:
-  x = 5
-    #whats x to 5?:
+    #answer the following comments:
+    x = 5
+        #whats x to 5?:
 
-  y = x
-  print(id(x))
-  print(id(y))
-  print("\n")
-  x = 1
-  print(id(x))
-  print(id(y))
-    #what do you notice about the ID's? 
+    y = x
+    print(id(x))
+    print(id(y))
+    print("\n")
+    x = 1
+    print(id(x))
+    print(id(y))
+    print("\n")
+        #what do you notice about the ID's? 
+    
+    my_list = [1, 2, 3, 4]
+    for i in my_list:
+        print(i)
 
-  list[1, 2, 3, 4]
-  for i in list
-    print(i)
+    print("\n")
+    change_list_values(my_list)
+    for i in my_list:
+        print(i)
 
-  change_list_values(list)
-  for i in list
-    print(i)
+    #what do you notice about index 1 of list?
   
+    #make a Food class with the follwoing
+        #meal of the day = string
+        #a constructor 
+    
+    #make the follwing, and print the results after each step
+        #pancakes, breakfast
+        #steak, Dinner
 
-  Pancakes = Food("Breakfast")
-  Steak = Food("Dinner)
+    #whats the difference between y = x and Steak = Pancakes
+
+if __name__ == '__main__':
+    main()
 
