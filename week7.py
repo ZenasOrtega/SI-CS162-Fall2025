c0813f7078466873a0af81230c2951b21e67a525
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-02-19 14:48:46 -0800

    Update week7.py

--- week7.py
+++ week7.py
@@ -1,1 +1,15 @@
-Class BankAccount:
+class Bank:
+  def action() -> None:
+    print("You dont even have an account")
+
+def main() -> None:
+    BankAccount = 100000
+    Answer = None
+    while(Answer != 3)
+      Answer = input("what would you like to do? 1.deposit 2.withdraw 3.Exit")
+      if(Answer !=3):
+        #make the withdraw and deposite actions that will change with BankAcciunt; Use the Override
+    
+
+if __name__ == '__main__':
+    main()
