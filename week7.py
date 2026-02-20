34ca00bec437afa5c9c903ff491fd707ae5a6db5
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-02-20 13:22:20 -0800

    Update week7.py

--- week7.py
+++ week7.py
@@ -1,15 +1,42 @@
-class Bank:
-  def action() -> None:
-    print("You dont even have an account")
+from typing import Generic, TypeVar
+from abc import ABC, abstractmethod
+
+class Animals:
+  Spc: str
+  Noise: str
+
+  def __init__(self, spc: str, sound: str) -> None:
+    self.Spc = spc
+    self.Noise = str
+
+  @abstractmethod
+  def noise(self) -> None:
+    pass
+
+#child classes here
+
+
+class Zoo:
+  _Animals: list[Animals] 
+
+  def __init__(self) -> None:
+    self._Animals = []
+
+  def print_zoo(self) -> None:
+    for i in self._Animals:
+      i.noise()
+
+  #add the method to be able to append a new animal to the Zoo's list
 
 def main() -> None:
-    BankAccount = 100000
-    Answer = None
-    while(Answer != 3)
-      Answer = input("what would you like to do? 1.deposit 2.withdraw 3.Exit")
-      if(Answer !=3):
-        #make the withdraw and deposite actions that will change with BankAcciunt; Use the Override
-    
+  Portland_zoo = Zoo()
+  #make a class for the following
+    #lion
+    #Turtle
+    #Monkey
+
+  #print the info out using the Zoo class method
+  
 
 if __name__ == '__main__':
     main()
