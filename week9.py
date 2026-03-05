5be2bce09a4c41ba528d50bf252f7a02d224ddd5
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-03-05 15:54:40 -0800

    Create week9.py

---
+++ week9.py
@@ -0,0 +1,28 @@
+#write how you will arrange the numbers in a list
+    #(hint: use 2 for loops)
+    #for loop 1
+      #
+        #for loop 2
+          #
+
+#lets start with our test funxtion
+  #first make a list of 5 non sorted ints
+  #now make a list using the same 5 numbers, but already sorted
+
+  #how will you test these 2 list together?
+
+def main():
+#make a function that allows a user to input 10 numbers
+  #this function will sort them from least to greatest 
+#answer the commented lines to help you build this function
+  
+#lets break down the code, answer in psuedo code:)
+  #look right above main
+  
+  #how will you loop through the 10 inputs
+    #
+
+
+if __name__ == "__main__":
+    main()
+  
