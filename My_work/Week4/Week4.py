75b1639e33abccf8597a516b200f03a29aafd955
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-03-26 18:36:50 -0700

    Update Week4.py

--- My_work/Week4/Week4.py
+++ My_work/Week4/Week4.py
@@ -1,8 +1,10 @@
 import car 
 
 def main() -> None:
+  #prompting the user
   user_wage = float(input('whats your monthly payment\n'))
 
+  #creating a object "McQueen" that is a "car" object
   McQueen = car.Car("Corvette", 4, 20000)
   McQueen.print_info()
 
