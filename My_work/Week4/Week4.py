de6cdec0b45f64df62faa99a74387c67d3b4944f
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-03-26 18:30:02 -0700

    Create Week4.py

---
+++ My_work/Week4/Week4.py
@@ -0,0 +1,15 @@
+import car 
+
+def main() -> None:
+  user_wage = float(input('whats your monthly payment\n'))
+
+  McQueen = car.Car("Corvette", 4, 20000)
+  McQueen.print_info()
+
+  car.monthly(user_wage, McQueen)
+  car.tire_condtions(McQueen)
+
+
+
+if __name__ == '__main__':
+  main()
