19bad1d331ed3cfe9291efae1ae92611bf0277b6
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-04-10 16:30:41 -0700

    Update Week4.py

--- My_work/Week4/Week4.py
+++ My_work/Week4/Week4.py
@@ -8,6 +8,8 @@ def main() -> None:
   McQueen = car.Car("Corvette", 4, 20000)
   McQueen.print_info()
 
+  #inside the "car" file, there are 2 functions we can access
+    #(name of the file).(functions you want to access)---
   car.monthly(user_wage, McQueen)
   car.tire_condtions(McQueen)
 
