a715a103826adc11bf4c777d3ae376863a9986ec
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-05-28 17:08:13 -0700

    Update week10.py

--- week10.py
+++ week10.py
@@ -1,1 +1,14 @@
-#searching and sorting
+import random
+
+#selection sort
+
+#insertion sort
+
+def main():
+    #ignore this, this is too print 10 ints into youre list
+    numbers = [random.randint(1, 100) for _ in range(10)]
+    print(numbers)
+
+    #make a function that sorts a list in the given way above, in both a for loop and recursive
+if __name__ == "__main__":
+    main()
