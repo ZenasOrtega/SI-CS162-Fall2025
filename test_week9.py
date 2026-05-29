097b1fa6013ce1f49cec1eda0b432d9f6ee00cdd
ZenasOrtega <166550933+ZenasOrtega@users.noreply.github.com>
2026-05-28 17:10:28 -0700

    Update test_week9.py

--- test_week9.py
+++ test_week9.py
@@ -1,15 +1,26 @@
-import random
+#write how you will arrange the numbers in a list
+    #(hint: use 2 for loops)
+    #for loop 1
+      #
+        #for loop 2
+          #
 
-#selection sort
-
-#insertion sort
+#lets start with our test funxtion
+  #first make a list of 5 non sorted ints
+  #now make a list using the same 5 numbers, but already sorted
+  
+#how will you test these 2 list together?
 
 def main():
-    #ignore this, this is too print 10 ints into youre list
-    numbers = [random.randint(1, 100) for _ in range(10)]
-    print(numbers)
-
-    #make a function that sorts a list in the given way above, in both a for loop and recursive
+    ##make a function that allows a user to input 10 numbers
+        #this function will sort them from least to greatest 
+    #answer the commented lines to help you build this function
+    
+    #lets break down the code, answer in psuedo code:)
+        #look right above main
+    
+        #how will you loop through the 10 inputs
+            #
 if __name__ == "__main__":
     main()
   
